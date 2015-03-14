9fece4246b25a7075e1802fd9890955ffc0611e4
ethers <ethereum@outlook.com>
2015-03-13 17:54:29 -0700

    verifyTx is in btcrelay.py

--- btcrelay/client/js/btcRelayAbi.js
+++ btcrelay/client/js/btcRelayAbi.js
@@ -1,18 +1,60 @@
 window.btcRelayAbi = [{
-    "name": "bulkStoreHeader(string,int256)",
+    "name": "getBlockchainHead()",
     "type": "function",
-    "inputs": [{ "name": "headersBinary", "type": "string" }, { "name": "count", "type": "int256" }],
+    "inputs": [],
     "outputs": [{ "name": "out", "type": "int256" }]
 },
 {
-    "name": "relayTx(string,int256,int256,int256[],int256[],int256,int256)",
+    "name": "getChainScore()",
     "type": "function",
-    "inputs": [{ "name": "txStr", "type": "string" }, { "name": "txHash", "type": "int256" }, { "name": "proofLen", "type": "int256" }, { "name": "hash", "type": "int256[]" }, { "name": "path", "type": "int256[]" }, { "name": "txBlockHash", "type": "int256" }, { "name": "contract", "type": "int256" }],
+    "inputs": [],
     "outputs": [{ "name": "out", "type": "int256" }]
 },
 {
-    "name": "setBtcRelay(int256)",
+    "name": "inMainChain(int256)",
     "type": "function",
-    "inputs": [{ "name": "btcRelayAddr", "type": "int256" }],
+    "inputs": [{ "name": "txBlockHash", "type": "int256" }],
+    "outputs": [{ "name": "out", "type": "int256" }]
+},
+{
+    "name": "saveAncestors(int256,int256)",
+    "type": "function",
+    "inputs": [{ "name": "blockHash", "type": "int256" }, { "name": "hashPrevBlock", "type": "int256" }],
+    "outputs": []
+},
+{
+    "name": "setRelayUtil(int256)",
+    "type": "function",
+    "inputs": [{ "name": "relayUtilAddr", "type": "int256" }],
+    "outputs": [{ "name": "out", "type": "int256" }]
+},
+{
+    "name": "storeBlockHeader(string)",
+    "type": "function",
+    "inputs": [{ "name": "blockHeaderBinary", "type": "string" }],
+    "outputs": [{ "name": "out", "type": "int256" }]
+},
+{
+    "name": "testingonlySetGenesis(int256)",
+    "type": "function",
+    "inputs": [{ "name": "blockHash", "type": "int256" }],
+    "outputs": []
+},
+{
+    "name": "testingonlySetHeaviest(int256)",
+    "type": "function",
+    "inputs": [{ "name": "blockHash", "type": "int256" }],
+    "outputs": []
+},
+{
+    "name": "verifyTx(int256,int256,int256[],int256[],int256)",
+    "type": "function",
+    "inputs": [{ "name": "tx", "type": "int256" }, { "name": "proofLen", "type": "int256" }, { "name": "hash", "type": "int256[]" }, { "name": "path", "type": "int256[]" }, { "name": "txBlockHash", "type": "int256" }],
+    "outputs": [{ "name": "out", "type": "int256" }]
+},
+{
+    "name": "within6Confirms(int256)",
+    "type": "function",
+    "inputs": [{ "name": "txBlockHash", "type": "int256" }],
     "outputs": [{ "name": "out", "type": "int256" }]
 }]
