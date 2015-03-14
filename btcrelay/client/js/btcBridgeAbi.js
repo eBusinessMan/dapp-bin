aee1c696036bc21a0070c3ea0223a3cfc45fb34f
ethers <ethereum@outlook.com>
2015-03-13 19:31:11 -0700

    manually fix btcBridgeAbi to bytes; call() probably doesn't work for relayTx anymore so need to debug in AZ the relayTx transaction

---
+++ btcrelay/client/js/btcBridgeAbi.js
@@ -0,0 +1,18 @@
+window.btcBridgeAbi = [{
+    "name": "bulkStoreHeader(bytes,int256)",
+    "type": "function",
+    "inputs": [{ "name": "headersBinary", "type": "bytes" }, { "name": "count", "type": "int256" }],
+    "outputs": [{ "name": "out", "type": "int256" }]
+},
+{
+    "name": "relayTx(bytes,int256,int256,int256[],int256[],int256,int256)",
+    "type": "function",
+    "inputs": [{ "name": "txStr", "type": "bytes" }, { "name": "txHash", "type": "int256" }, { "name": "proofLen", "type": "int256" }, { "name": "hash", "type": "int256[]" }, { "name": "path", "type": "int256[]" }, { "name": "txBlockHash", "type": "int256" }, { "name": "contract", "type": "int256" }],
+    "outputs": [{ "name": "out", "type": "int256" }]
+},
+{
+    "name": "setBtcRelay(int256)",
+    "type": "function",
+    "inputs": [{ "name": "btcRelayAddr", "type": "int256" }],
+    "outputs": [{ "name": "out", "type": "int256" }]
+}]
