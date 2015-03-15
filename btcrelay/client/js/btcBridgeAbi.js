28347fc3c5a5dd10f95c1920318663d2e30f5dda
ethers <ethereum@outlook.com>
2015-03-15 16:19:47 -0700

    verifyTx works with cpp built from develop branch pulled at timestamp 1426369865, and ethereum.js eceeda7; Serpent d35bb29; pyethereum f6f3b1a9d58

--- btcrelay/client/js/btcBridgeAbi.js
+++ btcrelay/client/js/btcBridgeAbi.js
@@ -1,3 +1,5 @@
+// TODO bulkStoreHeader first param change to string so that it's
+// consistent with relayTx(string...
 window.btcBridgeAbi = [{
     "name": "bulkStoreHeader(bytes,int256)",
     "type": "function",
@@ -5,7 +7,7 @@ window.btcBridgeAbi = [{
     "outputs": [{ "name": "out", "type": "int256" }]
 },
 {
-    "name": "relayTx(bytes,int256,int256,int256[],int256[],int256,int256)",
+    "name": "relayTx(string,int256,int256,int256[],int256[],int256,int256)",
     "type": "function",
     "inputs": [{ "name": "txStr", "type": "bytes" }, { "name": "txHash", "type": "int256" }, { "name": "proofLen", "type": "int256" }, { "name": "hash", "type": "int256[]" }, { "name": "path", "type": "int256[]" }, { "name": "txBlockHash", "type": "int256" }, { "name": "contract", "type": "int256" }],
     "outputs": [{ "name": "out", "type": "int256" }]
