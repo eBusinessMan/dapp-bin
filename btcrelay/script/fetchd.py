680ca194e589a752a6776b4f0b0513b100d2fae6
ethers <ethereum@outlook.com>
2015-06-03 13:35:54 -0700

    fetchd for trying to send ~3M gas txs to eth cpp; wasn't able to compete against others sending large txs it seems

--- btcrelay/script/fetchd.py
+++ btcrelay/script/fetchd.py
@@ -68,8 +68,8 @@ def main():
                 sleep(SLEEP_TIME)
             except Exception as e:
                 print(e)
-                print('Retry in 1min')
-                sleep(60)
+                print('Retry in 12s')
+                sleep(12)
                 continue
             except: # catch *all* exceptions
                 e = sys.exc_info()[0]
@@ -88,7 +88,10 @@ def run(doFetch=False, network=BITCOIN_TESTNET):
     # so that contract lost the correct HEAD.  we try 3 times since it would
     # be quite unlucky for 3 Ethereum reorgs to coincide with storing the
     # non-orphaned Bitcoin block
-    nTime = 3
+
+
+    # no need for fork retry since starting at 0
+    nTime = 0
     for i in range(nTime):
         # refetch if needed in case contract's HEAD was orphaned
         contractHeight = getLastBlockHeight()
@@ -131,7 +134,7 @@ def run(doFetch=False, network=BITCOIN_TESTNET):
 
     print('@@@ startFetch: {0} actualHeight: {1}').format(instance.heightToStartFetch, actualHeight)
 
-    chunkSize = 5
+    chunkSize = 17
     fetchNum =  actualHeight - instance.heightToStartFetch + 1
     numChunk = fetchNum / chunkSize
     leftoverToFetch = fetchNum % chunkSize
@@ -146,6 +149,9 @@ def run(doFetch=False, network=BITCOIN_TESTNET):
 
 def fetchHeaders(chunkStartNum, chunkSize, numChunk, network=BITCOIN_TESTNET):
     for j in range(numChunk):
+        if j>0 and j % 2 == 0:
+            raise NameError('Raise exception so cpp restarts fetch')
+
         strings = ""
         for i in range(chunkSize):
             blockNum = chunkStartNum  + i
@@ -185,7 +191,7 @@ def storeHeaders(bhBinary, chunkSize):
     # bhBinary = '\x02\x00\x00\x00~\xf0U\xe1gM.eQ\xdb\xa4\x1c\xd2\x14\xde\xbb\xee4\xae\xb5D\xc7\xecg\x00\x00\x00\x00\x00\x00\x00\x00\xd3\x99\x89c\xf8\x0c[\xabC\xfe\x8c&"\x8e\x98\xd00\xed\xf4\xdc\xbeH\xa6f\xf5\xc3\x9e-z\x88\\\x91\x02\xc8mSl\x89\x00\x19Y:G\r\x02\x00\x00\x00Tr\xac\x8b\x11\x87\xbf\xcf\x91\xd6\xd2\x18\xbb\xda\x1e\xb2@]|U\xf1\xf8\xcc\x82\x00\x00\x00\x00\x00\x00\x00\x00\xab\n\xaa7|\xa3\xf4\x9b\x15E\xe2\xaek\x06g\xa0\x8fB\xe7-\x8c$\xae#q@\xe2\x8f\x14\xf3\xbb|k\xccmSl\x89\x00\x19\xed\xd8<\xcf\x02\x00\x00\x00\xa9\xab\x12\xe3,\xed\xdc+\xa5\xe6\xade\x1f\xacw,\x986\xdf\x83M\x91\xa0I\x00\x00\x00\x00\x00\x00\x00\x00\xdfuu\xc7\x8f\x83\x1f \xaf\x14~\xa7T\xe5\x84\xaa\xd9Yeiic-\xa9x\xd2\xddq\x86#\xfd0\xc5\xccmSl\x89\x00\x19\xe6Q\x07\xe9\x02\x00\x00\x00,P\x1f\xc0\xb0\xfd\xe9\xb3\xc1\x0e#S\xc1TI*5k\x1a\x02)^+\x86\x00\x00\x00\x00\x00\x00\x00\x00\xa7\xaaa\xc8\xd3|\x88v\xba\xa0\x17\x9ej2\x94D4\xbf\xd3\xe1\xccug\x89*1K\x0c{\x9e]\x92\'\xcemSl\x89\x00\x19\xa4\xa0<{\x02\x00\x00\x00\xe7\xfc\x91>+y\n0v\x0c\xaa\xfb\x9b_\xaa\xe1\xb5\x1dlT\xff\xe4\xae\x82\x00\x00\x00\x00\x00\x00\x00\x00P\xad\x11k\xfb\x11c\x03\x03a\xd9}H\xb4\xca\x90\'\xa4\x9b\xca\xf8\xb8\xd4!\x1b\xaa\x92\xccr\xe7\xe1#f\xcfmSl\x89\x00\x19\xe6\x13\x9c\x82'
     data = [bhBinary, chunkSize]
 
-    gas = 900000
+    gas = 3000000
     value = 0
 
 
@@ -200,10 +206,10 @@ def storeHeaders(bhBinary, chunkSize):
     instance.transact(instance.relayContract, fun_name=fun_name, sig=sig,
         data=data, gas=gas, gas_price=instance.gasPrice, value=value)
 
-    instance.wait_for_transaction(
-        from_count=from_count,
-        #verbose=(True if api_config.get('misc', 'verbosity') > 1 else False))
-        verbose=True)
+    # instance.wait_for_transaction(
+    #     from_count=from_count,
+    #     #verbose=(True if api_config.get('misc', 'verbosity') > 1 else False))
+    #     verbose=True)
 
 
     # waitTxRes = instance.wait_for_transaction(
